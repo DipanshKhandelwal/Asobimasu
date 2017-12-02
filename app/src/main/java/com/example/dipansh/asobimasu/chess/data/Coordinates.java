279c094ea2b34c9cc5387ef96ac6bb2d06178a6c
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-12-02 09:59:31 +0530

    Added class coordinates

---
+++ app/src/main/java/com/example/dipansh/asobimasu/chess/data/Coordinates.java
@@ -0,0 +1,31 @@
+package com.example.dipansh.asobimasu.chess.data;
+
+/**
+ * Created by DIPANSH KHANDELWAL on 03-06-2017
+ */
+
+public class Coordinates {
+    private int x;
+    private int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
